0097885fd837e7055d266756f40ed949cb23dd6c
Aras <amvitkus@gmail.com>
2022-07-08 12:32:00 -0400

    Added splashBinding to SplashActivity.kt. Updated AndroidManifest.xml to use intent for splash screen to launch first.

--- app/src/main/java/com/arasvitkus/mesbglister/SplashActivity.kt
+++ app/src/main/java/com/arasvitkus/mesbglister/SplashActivity.kt
@@ -2,10 +2,15 @@ package com.arasvitkus.mesbglister
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.arasvitkus.mesbglister.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
     }
 }
