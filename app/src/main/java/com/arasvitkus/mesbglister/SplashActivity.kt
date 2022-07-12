4f3293762e7598567804bb53ade128d509b86da5
Aras <amvitkus@gmail.com>
2022-07-12 12:17:32 -0400

    Check SDK version in SplashActivity.kt. Added way to check for deprecation.

--- app/src/main/java/com/arasvitkus/mesbglister/SplashActivity.kt
+++ app/src/main/java/com/arasvitkus/mesbglister/SplashActivity.kt
@@ -1,8 +1,11 @@
 package com.arasvitkus.mesbglister
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.arasvitkus.mesbglister.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -12,5 +15,15 @@ class SplashActivity : AppCompatActivity() {
 
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars()) // Hide status bar
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
