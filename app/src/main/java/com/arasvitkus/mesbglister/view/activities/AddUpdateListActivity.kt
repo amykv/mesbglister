f761ede522ca52effa5ff7f03a798ec1af190c7f
Aras <amvitkus@gmail.com>
2022-07-13 11:19:11 -0400

    More MVVM refactoring. Created AddUpdateListActivity.kt.

---
+++ app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
@@ -0,0 +1,12 @@
+package com.arasvitkus.mesbglister.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arasvitkus.mesbglister.R
+//Taking care of adding and updating new army list
+class AddUpdateListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_list)
+    }
+}
