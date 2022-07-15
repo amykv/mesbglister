86451c13e75fd12ef236993a78d3b4d349c5c889
Aras <amvitkus@gmail.com>
2022-07-15 11:07:45 -0400

    Added method in AddUpdateListActivity.kt to hold toolbar with back button. Made it to use view binding. Update AndroidManifest.xml to force vertical orientation.

--- app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
+++ app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
@@ -3,10 +3,31 @@ package com.arasvitkus.mesbglister.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.arasvitkus.mesbglister.R
+import com.arasvitkus.mesbglister.databinding.ActivityAddUpdateListBinding
+
 //Taking care of adding and updating new army list
 class AddUpdateListActivity : AppCompatActivity() {
+
+    //Allows whole class to use view binging. Allows to setup action bar
+    private lateinit var mBinding: ActivityAddUpdateListBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_list)
+        //mBinding should be activity that is inflated
+        mBinding = ActivityAddUpdateListBinding.inflate(layoutInflater)
+
+        setContentView(mBinding.root)
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar(){
+        //Assigning the toolbar
+        setSupportActionBar(mBinding.toolbarAddArmyActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        mBinding.toolbarAddArmyActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
