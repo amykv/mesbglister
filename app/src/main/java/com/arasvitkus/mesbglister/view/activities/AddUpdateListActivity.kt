2fb389d3d26814a014df5954bfabaa434953bffb
Aras <amvitkus@gmail.com>
2022-07-15 11:50:56 -0400

    Added vector images for camera and gallery when user will add photo to an army list. Created methods to bind and inflate custom dialog for camera/gallery selection. Updated strings.xml.

--- app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
+++ app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
@@ -1,12 +1,16 @@
 package com.arasvitkus.mesbglister.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.arasvitkus.mesbglister.R
 import com.arasvitkus.mesbglister.databinding.ActivityAddUpdateListBinding
+import com.arasvitkus.mesbglister.databinding.DialogCustomImageSelectionBinding
 
 //Taking care of adding and updating new army list
-class AddUpdateListActivity : AppCompatActivity() {
+class AddUpdateListActivity : AppCompatActivity(), View.OnClickListener {
 
     //Allows whole class to use view binging. Allows to setup action bar
     private lateinit var mBinding: ActivityAddUpdateListBinding
@@ -20,6 +24,9 @@ class AddUpdateListActivity : AppCompatActivity() {
         setContentView(mBinding.root)
 
         setupActionBar()
+
+        //Assign onclicklistener to iv army image
+        mBinding.ivAddArmyImage.setOnClickListener (this)
     }
 
     private fun setupActionBar(){
@@ -29,5 +36,34 @@ class AddUpdateListActivity : AppCompatActivity() {
         mBinding.toolbarAddArmyActivity.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_add_army_image ->{
+                    customImageSelectionDialog()
+                    return
+                }
+            }
+        }
+    }
+
+    private fun customImageSelectionDialog(){
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this, "Gallery Clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
