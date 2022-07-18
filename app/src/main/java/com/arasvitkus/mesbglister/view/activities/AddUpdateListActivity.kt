d0a56714fd1b56ad8d13523038d144f7d9a5b76a
Aras <amvitkus@gmail.com>
2022-07-18 12:22:10 -0400

    Created edit icon. User is able to take photo or use gallery for adding an image in AddUpdateListActivity.kt. Permissions handled using Dexter.

--- app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
+++ app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
@@ -11,12 +11,18 @@ import com.arasvitkus.mesbglister.databinding.DialogCustomImageSelectionBinding
 import com.karumi.dexter.Dexter
 import android.Manifest
 import android.Manifest.permission.CAMERA
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -32,6 +38,8 @@ class AddUpdateListActivity : AppCompatActivity(), View.OnClickListener {
 
     //Allows whole class to use view binging. Allows to setup action bar
     private lateinit var mBinding: ActivityAddUpdateListBinding
+    private lateinit var resultLauncherCamera: ActivityResultLauncher<Intent>
+    private lateinit var galleryImageResultLauncher: ActivityResultLauncher<Intent>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,6 +53,7 @@ class AddUpdateListActivity : AppCompatActivity(), View.OnClickListener {
 
         //Assign onclicklistener to iv army image
         mBinding.ivAddArmyImage.setOnClickListener (this)
+        onActivityResult()
     }
 
     private fun setupActionBar(){
@@ -67,6 +76,52 @@ class AddUpdateListActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    private fun onActivityResult(){
+        resultLauncherCamera =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+
+                    data?.extras?.let {
+                        val thumbnail: Bitmap =
+                            data.extras!!.get("data") as Bitmap // Bitmap from camera
+                        mBinding.ivArmyImage.setImageBitmap(thumbnail) // Set to the imageView.
+
+                        // Replace the add icon with edit icon once the image is selected.
+                        mBinding.ivAddArmyImage.setImageDrawable(
+                            ContextCompat.getDrawable(
+                                this@AddUpdateListActivity,
+                                R.drawable.ic_vector_edit
+                            )
+                        )
+                    }
+                }else if (result.resultCode == Activity.RESULT_CANCELED) {
+                    Log.e("Cancelled", "Cancelled")
+                }
+            }
+        galleryImageResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val data: Intent? = result.data
+                    data?.let {
+                        val selectedPhotoUri = data.data
+
+                        mBinding.ivArmyImage.setImageURI(selectedPhotoUri)
+                        mBinding.ivAddArmyImage.setImageDrawable(
+                            ContextCompat.getDrawable(
+                                this@AddUpdateListActivity,
+                                R.drawable.ic_vector_edit
+                            )
+                        )
+                    }
+                }else if (result.resultCode == Activity.RESULT_CANCELED) {
+                    Log.e("Cancelled", "Cancelled")
+                }
+            }
+    }
+
     private fun customImageSelectionDialog(){
         val dialog = Dialog(this)
         val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
@@ -74,79 +129,81 @@ class AddUpdateListActivity : AppCompatActivity(), View.OnClickListener {
 
         //Implementing Dexter library to get permissions.
         binding.tvCamera.setOnClickListener {
-            Dexter.withContext(this).withPermissions(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-               // Manifest.permission.WRITE_EXTERNAL_STORAGE, commenting this out because not necessary for newer API levels
-                Manifest.permission.CAMERA
-            ).withListener(object: MultiplePermissionsListener{
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    //Report is not empty, then let this code execute...
-                    report?.let {
-                    if (report.areAllPermissionsGranted()) {
-                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)//Allows to capture image
-                        //startActivityForResult(intent, CAMERA)
+            Dexter.withContext(this@AddUpdateListActivity)
+                .withPermissions(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.CAMERA
+                )
+                .withListener(object : MultiplePermissionsListener {
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()) {
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            resultLauncherCamera.launch(intent)
+                        }
                     }
-                }
-                }
+                    override fun onPermissionRationaleShouldBeShown(
+                        permissions: MutableList<PermissionRequest>?,
+                        token: PermissionToken?
+                    ) {
 
-                override fun onPermissionRationaleShouldBeShown(
-                    permissions: MutableList<PermissionRequest>?,
-                    token: PermissionToken?
-                ) {
-                    showRationalDialogForPermissions()
-                }
+                        showRationalDialogForPermissions()
 
-            }
-
-            ).onSameThread().check() //Asking multiple permissions using Dexter,
+                    }
+                }).onSameThread()
+                .check()
             dialog.dismiss()
         }
-
-        //Single permission for Gallery, NOT using PERMISSIONS, use PERMISSION
         binding.tvGallery.setOnClickListener {
-            Dexter.withContext(this).withPermission(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                //Manifest.permission.WRITE_EXTERNAL_STORAGE, not necessary for newer API levels
-            ).withListener(object: PermissionListener {
-                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                    Toast.makeText(this@AddUpdateListActivity, "You have gallery permission to select an image.", Toast.LENGTH_SHORT).show()
-                }
+            Dexter.withContext(this@AddUpdateListActivity)
+                .withPermission(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+                .withListener(object : PermissionListener {
+                    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+                        val galleryIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+                        galleryImageResultLauncher.launch(galleryIntent)
+                    }
 
-                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
-                    Toast.makeText(this@AddUpdateListActivity, "You have denied storage permission.", Toast.LENGTH_SHORT).show()
-                }
+                    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+                        Toast.makeText(
+                            this@AddUpdateListActivity,
+                            "You have denied the storage permission to select image.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
 
-                override fun onPermissionRationaleShouldBeShown(
-                    p0: PermissionRequest?,
-                    p1: PermissionToken?
-                ) {
-                    showRationalDialogForPermissions()
-                }
+                    override fun onPermissionRationaleShouldBeShown(
+                        permission: PermissionRequest?,
+                        token: PermissionToken?
+                    ) {
+                        showRationalDialogForPermissions()
+                    }
 
-            }
-
-            ).onSameThread().check() //Asking multiple permissions using Dexter,
+                }).onSameThread()
+                .check()
             dialog.dismiss()
         }
-
         dialog.show()
     }
-
-    //Method if user says no/no access to camera permission
     private fun showRationalDialogForPermissions() {
-        AlertDialog.Builder(this).setMessage("Permissions are turned off for this feature. Enable this under application settings.")
-            .setPositiveButton("Go to settings")
-            {_,_ ->
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ -> //lambda
                 try {
                     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                     val uri = Uri.fromParts("package", packageName, null)
                     intent.data = uri
                     startActivity(intent)
-                }catch(e: ActivityNotFoundException){
+                } catch (e: ActivityNotFoundException) {
                     e.printStackTrace()
                 }
             }
-            .setNegativeButton("Cancel"){dialog, _ ->
+            .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }.show()
     }
