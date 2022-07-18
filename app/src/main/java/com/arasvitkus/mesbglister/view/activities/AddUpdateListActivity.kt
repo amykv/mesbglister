8d5d8661e42a539cef2650ad2d253be897520b1f
Aras <amvitkus@gmail.com>
2022-07-18 11:37:47 -0400

    Methods added for getting Camera and Gallery storage permissions.

--- app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
+++ app/src/main/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivity.kt
@@ -8,6 +8,24 @@ import android.widget.Toast
 import com.arasvitkus.mesbglister.R
 import com.arasvitkus.mesbglister.databinding.ActivityAddUpdateListBinding
 import com.arasvitkus.mesbglister.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import android.Manifest
+import android.Manifest.permission.CAMERA
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
+import android.provider.Settings
+import androidx.core.content.ContextCompat.startActivity
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
+import java.nio.channels.FileChannel
 
 //Taking care of adding and updating new army list
 class AddUpdateListActivity : AppCompatActivity(), View.OnClickListener {
@@ -54,16 +72,82 @@ class AddUpdateListActivity : AppCompatActivity(), View.OnClickListener {
         val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
 
+        //Implementing Dexter library to get permissions.
         binding.tvCamera.setOnClickListener {
-            Toast.makeText(this, "Camera Clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+               // Manifest.permission.WRITE_EXTERNAL_STORAGE, commenting this out because not necessary for newer API levels
+                Manifest.permission.CAMERA
+            ).withListener(object: MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    //Report is not empty, then let this code execute...
+                    report?.let {
+                    if (report.areAllPermissionsGranted()) {
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)//Allows to capture image
+                        //startActivityForResult(intent, CAMERA)
+                    }
+                }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }
+
+            ).onSameThread().check() //Asking multiple permissions using Dexter,
             dialog.dismiss()
         }
 
+        //Single permission for Gallery, NOT using PERMISSIONS, use PERMISSION
         binding.tvGallery.setOnClickListener {
-            Toast.makeText(this, "Gallery Clicked", Toast.LENGTH_SHORT).show()
+            Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                //Manifest.permission.WRITE_EXTERNAL_STORAGE, not necessary for newer API levels
+            ).withListener(object: PermissionListener {
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    Toast.makeText(this@AddUpdateListActivity, "You have gallery permission to select an image.", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(this@AddUpdateListActivity, "You have denied storage permission.", Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+
+            }
+
+            ).onSameThread().check() //Asking multiple permissions using Dexter,
             dialog.dismiss()
         }
 
         dialog.show()
+    }
+
+    //Method if user says no/no access to camera permission
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this).setMessage("Permissions are turned off for this feature. Enable this under application settings.")
+            .setPositiveButton("Go to settings")
+            {_,_ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch(e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
