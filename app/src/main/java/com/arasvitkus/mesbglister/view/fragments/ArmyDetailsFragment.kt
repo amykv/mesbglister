68b5c924e5a424c4b5dbba659a711da6aa6e46c7
Aras <amvitkus@gmail.com>
2022-07-25 12:45:41 -0400

    Added Android Palette to be used when user selects an army already created. Updated ArmyDetailsFragment.kt method to use palette. Updated build.gradle for palette.

--- app/src/main/java/com/arasvitkus/mesbglister/view/fragments/ArmyDetailsFragment.kt
+++ app/src/main/java/com/arasvitkus/mesbglister/view/fragments/ArmyDetailsFragment.kt
@@ -1,15 +1,22 @@
 package com.arasvitkus.mesbglister.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.arasvitkus.mesbglister.R
 import com.arasvitkus.mesbglister.databinding.FragmentArmyDetailsBinding
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import java.io.IOException
 import java.util.*
 
@@ -48,6 +55,36 @@ class ArmyDetailsFragment : Fragment() {
                 Glide.with(requireActivity())
                     .load(it.armyDetails.image)
                     .centerCrop()
+                    .listener(object: RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            Log.e("TAG", "Error loading image", e)
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            resource.let {
+                                Palette.from(resource!!.toBitmap()).generate(){
+                                        palette ->
+                                    val intColor = palette?.vibrantSwatch?.rgb ?: 0 // Take vibrantSwatch rgb if it exists, if not, take zero
+                                    mBinding!!.rlArmyDetailMain.setBackgroundColor(intColor)
+                                 }
+
+                            }
+                            return false
+                        }
+
+                    })
                     .into(mBinding!!.ivArmyImage)
             }catch (e: IOException){
                 e.printStackTrace()
