24054f02bb0b5196c4d1d01493e5dbfe5fce3eff
Aras <amvitkus@gmail.com>
2022-08-01 17:49:41 -0400

    Documentation updated.

--- app/src/main/java/com/arasvitkus/mesbglister/model/database/MesbgListerRepository.kt
+++ app/src/main/java/com/arasvitkus/mesbglister/model/database/MesbgListerRepository.kt
@@ -16,8 +16,8 @@ import kotlinx.coroutines.flow.Flow
 class MesbgListerRepository(private val mesbgListerDao: MesbgListerDao) {
 
     /**
-     * By default Room runs suspend queries off the main thread, therefore, we don't need to
-     * implement anything else to ensure we're not doing long running database work
+     * By default Room runs suspend queries off the main thread, therefore, don't need to
+     * implement anything else to ensure it's not doing long running database work
      * off the main thread.
      */
     @Suppress("RedundantSuspendModifier")
