aec5217c9efc6618627788d720781416f5c72be2
Aras <amvitkus@gmail.com>
2022-08-01 17:46:36 -0400

    Added some unit test boilerplate code.

---
+++ app/src/test/java/com/arasvitkus/mesbglister/view/activities/AddUpdateListActivityTest.kt
@@ -0,0 +1,15 @@
+package com.arasvitkus.mesbglister.view.activities
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class AddUpdateListActivityTest {
+
+    private lateinit var addUpdateListActivity: AddUpdateListActivity
+
+    @Before
+    fun setUp() {
+        addUpdateListActivity = AddUpdateListActivity()
+    }
+
+}
---
+++ app/src/test/java/com/arasvitkus/mesbglister/view/activities/datarepository/FakeRepository.kt
@@ -0,0 +1,12 @@
+package com.arasvitkus.mesbglister.view.activities.datarepository
+
+import com.arasvitkus.mesbglister.model.database.MesbgListerDao
+import com.arasvitkus.mesbglister.model.database.MesbgListerRepository
+import com.arasvitkus.mesbglister.model.entities.MesbgLister
+
+class FakeRepository {
+
+    //override suspend fun insertMesbgListerData(mesbgLister: MesbgLister){
+    //mesbgListerDao.insertMesbgListerDetails(mesbgLister)
+    //}
+}
